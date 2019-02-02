cca2ced1515a07d8a47cb339b244a1e323073581
Carlo Panzi <carlo.panzi@gmail.com>
2019-02-02 14:46:29 +0100

    build: Moved tests to ava

--- tests/activate.js
+++ tests/activate.js
@@ -1,25 +1,21 @@
-const test = require('tape');
-const { JSDOM } = require("jsdom");
-const $k = require('..');
+import test from "ava";
+const $k = require("..");
 
+test("basic controller test", function(t) {
+  document.body.innerHTML = `<div>
+    <div controller="hallo">
+      <p>Hello world</p>
+    </div>
+    <p>Other Text</p>
+  </div>`;
 
-const { window } = new JSDOM(`<!DOCTYPE html>
-	<html>
-		<body>
-			<div controller="hallo"><p>Hello world</p></div>
-			<p>Other Text</p>
-		</body>
-	</html>`);
-global.document = window.document
-
-test('basic controller test', function (t) {
-    
-    
-    $k.activate('hallo', ($, element) => {
-    	t.ok(typeof element === 'object', 'Controller element is defined');
-    	t.equal(element.textContent, 'Hello world', 'Element inner text is Hello world');
-    	t.equal($('p').length, 1, 'Local selector finds 1 p element');
-    	t.end();
-    })
-   
+  $k.activate("hallo", ($, element) => {
+    t.true(typeof element === "object", "Controller element is defined");
+    t.is(
+      element.textContent.trim(),
+      "Hello world",
+      "Element inner text is Hello world"
+    );
+    t.is($("p").length, 1, "Local selector finds 1 p element");
+  });
 });
